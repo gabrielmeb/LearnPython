7fe2480113cd42cd200a16b2ead01180ca5be9dc
gabrielmeb <gabrielmebratu123@hotmail.com>
2024-02-08 15:42:07 -0500

    completed both numbers guessing game and madlibs game

---
+++ madlibs/madlibs.py
@@ -0,0 +1,13 @@
+# # Beginner project for making a madlibs program using basic python
+# myName = "Gabriel Mebratu"
+# print(f"I love {myName}")
+# Small example on how to concantenate in python
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("Verb: ")
+celebrity = input("Celebrity: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because  I love to {verb1}. Stay hydrated and {verb2} like you are {celebrity}!"
+
+print(madlib)
